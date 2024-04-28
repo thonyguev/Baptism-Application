016c9d46e16b5bf6ee98d22ec3ab47053d08263a
Manuel Guevara <thonyguev-github@outlook.com>
2024-04-27 23:44:51 -0500

    add: datatable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { DataTable } from "@/components/datatable/data-table";
+
 export default function Page() {
   return (
     <div>
       <h2 className="font-bold text-xl m-4">Bautizados</h2>
+      <DataTable />
     </div>
   );
 }
---
+++ src/components/datatable/columns.tsx
@@ -0,0 +1,88 @@
+import { ColumnDef } from "@tanstack/react-table";
+import { Checkbox } from "../ui/checkbox";
+import { CaretSortIcon, DotsHorizontalIcon } from "@radix-ui/react-icons";
+import { Button } from "../ui/button";
+import { BaptismPerson } from "./data-table";
+
+export const columns: ColumnDef<BaptismPerson>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "fullName",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Nombre Completo
+          <CaretSortIcon className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => (
+      <div className="capitalize">{row.getValue("fullName")}</div>
+    ),
+  },
+  {
+    accessorKey: "godmotherName",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Madrina
+          <CaretSortIcon className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => (
+      <div className="capitalize">{row.getValue("godmotherName")}</div>
+    ),
+  },
+  {
+    accessorKey: "godfatherName",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Padrino
+          <CaretSortIcon className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => (
+      <div className="capitalize">{row.getValue("godfatherName")}</div>
+    ),
+  },
+  {
+    accessorKey: "ministerOfTheSacrament",
+    header: "Ministro del Sacramento",
+    cell: ({ row }) => (
+      <div className="capitalize">{row.getValue("ministerOfTheSacrament")}</div>
+    ),
+  },
+];
